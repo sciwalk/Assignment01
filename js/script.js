ed253746f7bc7714c7773460092e1302fb7d38e7
David W <111401603+sciwalk@users.noreply.github.com>
2024-02-21 18:39:36 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// STEP 1
+// STEP 2
+// STEP 3
+// STEP 4
+// STEP 5
+// STEP 6
+// STEP 7
+// STEP 8
+// STEP 9
+// STEP .0
+// STEP .1
+// STEP .2
+// STEP
+// STEP
+// STEP
+// STEP
+// STEP
+// STEP
+// STEP
+// STEP
