88f50a7fdc0cad0e0b998b90a46cb11508caa03e
David W <111401603+sciwalk@users.noreply.github.com>
2024-02-21 18:49:20 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-// STEP 1
+// STEP 1 - convert identifiers to Camel Case notation:
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
 // STEP 2
 // STEP 3
 // STEP 4
@@ -10,11 +16,13 @@
 // STEP .0
 // STEP .1
 // STEP .2
-// STEP
-// STEP
-// STEP
-// STEP
-// STEP
-// STEP
-// STEP
-// STEP
+// STEP .3
+// STEP .4
+// STEP .5
+// STEP .6
+// STEP .7
+// STEP .8
+// STEP .9
+// STEP  ;0
+// STEP  ;1
+// STEP ;2
