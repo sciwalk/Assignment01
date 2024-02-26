b6bf4823c5d94c3d304bc097918759921ba3656b
David W <111401603+sciwalk@users.noreply.github.com>
2024-02-26 05:16:56 -0800

    commit step 5

--- js/script.js
+++ js/script.js
@@ -16,22 +16,48 @@
 // let anExpression = 3 + 4
 // let anotherExpression = anExpression * 2
 // STEP 4 - Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
-let strFirstName, strLastName, strAddress, strCity, strState, strZipCode, intYourAge, strReferralSource, strMayWeContactYou
-// STEP 5
-// STEP 6
-// STEP 7
-// STEP 8
-// STEP 9
-// STEP .0
-// STEP .1
-// STEP .2
-// STEP .3
-// STEP .4
-// STEP .5
-// STEP .6
-// STEP .7
-// STEP .8
-// STEP .9
-// STEP  ;0
-// STEP  ;1
-// STEP ;2
+//
+// let strFirstName, strLastName, strAddress, strCity, strState, strZipCode, intYourAge, strReferralSource, blnMayWeContactYou
+// STEP 5 Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+// 1.
+let strFirstName
+strFirstName = 'Dave'
+// 2. 
+let strCity = 'San Diego'
+// 3. 
+let strCity = 'San Diego', strState = 'CA'
+
+// STEP 6 Create a variable. Add a number and a string and display the coerced result in the browser’s console window.
+// STEP 7 Create two variables. For the first variable, add a Boolean and a string and display the coerced result. For the second variable, add a number and a Boolean and display the coerced result.
+// STEP 8 Is the following string literal valid? If not, how would you fix it? let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'console.log(someString)
+// STEP 9 Create a variable that produces a null value in the console window. Then, create a variable that produces an undefined value in the console window.
+// STEP .0 Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+// STEP .1 Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows:
+
+// Hello Zak Ruvalcaba, welcome to the JavaScript class!
+
+// Substitute my name for your name. Although not necessary in practice, I want you to use 2 concatenation operators to construct this string of text. One after the text “Hello” and a second one after your name and before the comma.
+// STEP .2 Declare a variable called name and set it equal to your name.
+// Substitute your name in the previous alert string with the variable instead.
+// STEP .3 Declare a variable called course and set it equal to “JavaScript”.
+// Rework your alert string so that it displays the string of text but using the variables as opposed to hard coded text.
+// STEP .4 Rework the above string so that a line break is added right before the word “Welcome”. Your alert box should display as follows:
+
+// Hello Zak Ruvalcaba.
+// Welcome to the JavaScript class!
+// STEP .5 Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable.
+// STEP .6 Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+// STEP .7 Declare a variable called x and assign it a value of 10.
+// Declare a variable called y and assign it a value of 20.
+// Display the sum of those two numbers in the console window.
+// STEP .8 Declare a variable called x and assign it a value of 20.
+// Add and assign 20 to that variable and display the result in the console window.
+// The result should be 40.
+// STEP .9 Declare a variable called x and assign it a value of 20.
+// Multiply and assign 5 to that variable and display the result in the console window.
+// The result should be 100.
+// STEP  ;0 Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.
+// Divide and assign 1 to that variable and display the result in the console window.
+// The result should be 2. If you got 6.66 try again.
+// STEP  ;1 Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window.
+// STEP ;2 Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
