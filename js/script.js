63cc57be70360dc80fab0d2c67a3fb025e8acef5
David W <111401603+sciwalk@users.noreply.github.com>
2024-02-27 06:36:26 -0800

    commit step .8

--- js/script.js
+++ js/script.js
@@ -84,8 +84,10 @@
 // Display the sum of those two numbers in the console window.
 // console.log(x + y)
 // STEP .8 Declare a variable called x and assign it a value of 20.
-// let x = 20
+let x = 20
 // Add and assign 20 to that variable and display the result in the console window.
+x += 20
+console.log(x)
 // The result should be 40.
 // STEP .9 Declare a variable called x and assign it a value of 20.
 // Multiply and assign 5 to that variable and display the result in the console window.
